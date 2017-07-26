b468e922367aa5afa0da3868fe13c633d8f7f20c
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2017-07-26 18:40:54 +0200

    Wyczyściłem wszystko ]:)

--- cstest/cstest/Program.cs
+++ cstest/cstest/Program.cs
@@ -3,34 +3,10 @@ using System.Collections.Generic;
 
 namespace cstest
 {
-    public class Human
-    {
-        
-        public Human(string imie, int wiek)
-        {
-            this.imie = imie;
-            this.wiek = wiek;
-        }
-
-        public string imie;
-        public int wiek;
-
-        public void sayHi()
-        {
-            Console.WriteLine($"Czesc, jestem {imie} i mam {wiek} lat.");
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            List<Human> humans = new List<Human>();
-            //Human h1 = new Human("Kazik", 17);
-            humans.Add(new Human("Kazik", 17));
-            humans.ForEach(c => c.sayHi());
-
-            Console.ReadKey();
         }
     }
 }
